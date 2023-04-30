a580e42dadd7c72fa3713d3a2a47f703eb92f349
Rodolpho <redsmedeiros@gmail.com>
2023-04-30 08:35:30 -0300

    criando o serviço de registrar usuario

--- src/main/java/com/rodolpho/projetothais/service/AuthService.java
+++ src/main/java/com/rodolpho/projetothais/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.rodolpho.projetothais.service;
 
 import com.rodolpho.projetothais.payload.LoginDto;
+import com.rodolpho.projetothais.payload.RegisterDto;
 
 public interface AuthService {
     
     String login(LoginDto loginDto);
+
+    String register(RegisterDto registerDto);
 }
--- src/main/java/com/rodolpho/projetothais/service/impl/AuthServiceImpl.java
+++ src/main/java/com/rodolpho/projetothais/service/impl/AuthServiceImpl.java
@@ -1,13 +1,24 @@
 package com.rodolpho.projetothais.service.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.rodolpho.projetothais.entity.Role;
+import com.rodolpho.projetothais.entity.User;
+import com.rodolpho.projetothais.exception.NurseAPIException;
+import com.rodolpho.projetothais.exception.ResourceNotFoundException;
 import com.rodolpho.projetothais.payload.LoginDto;
+import com.rodolpho.projetothais.payload.RegisterDto;
+import com.rodolpho.projetothais.repository.RoleRepository;
+import com.rodolpho.projetothais.repository.UserRepository;
 import com.rodolpho.projetothais.service.AuthService;
 
 @Service
@@ -15,9 +26,21 @@ public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager){
+    private UserRepository userRepository;
+
+    private RoleRepository roleRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder ){
 
         this.authenticationManager = authenticationManager;
+
+        this.userRepository = userRepository;
+
+        this.roleRepository = roleRepository;
+
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,6 +51,37 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         
         return "User logged-in successfully";
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+
+        if(userRepository.existsByUsername(registerDto.getUsername())){
+            throw new NurseAPIException(HttpStatus.BAD_REQUEST, "Já cadastrado");
+        }
+
+        if(userRepository.existsByEmail(registerDto.getEmail())){
+            throw new NurseAPIException(HttpStatus.BAD_REQUEST, "Já cadastrado");
+        }
+
+        User user = new User();
+
+        user.setName(registerDto.getName());
+        user.setEmail(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+
+        roles.add(userRole);
+
+        user.setRoles(roles);
+
+        userRepository.save(user);
+       
+        return "Usuário registrado";
     }
     
 }
